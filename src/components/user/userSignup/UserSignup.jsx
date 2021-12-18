3b4ef0311387370fd6e5de13ebfc661f6d84cab0
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2021-12-18 10:15:07 +0530

    make the auth login signup and logout

--- src/components/user/userSignup/UserSignup.jsx
+++ src/components/user/userSignup/UserSignup.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-
+import React,{useState}from 'react'
+import {supabase }from '../../../supabaseClient' 
 export const UserSignup = () => {
+  const [email,setEmail]=useState('')
+  const[password,setPassword]=useState('')
+
+  const Signup =async()=>{
+     // eslint-disable-next-line
+    const { user,  error } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+    })
+  }
     return (
       <>
 
-          <h1>This is User Signup</h1>
-      
+          <input type="email" placeholder='email' onChange={e=>setEmail(e.target.value)}/>
+          <input type="password" placeholder='password' onChange={e=>setPassword(e.target.value)}/>
+        <button type='summit' onClick={Signup}> signup</button>
       </>
     )
 }
