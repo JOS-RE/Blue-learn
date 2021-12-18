163a0683117ba6d7ba4a26108a4122581d81a572
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2021-12-18 08:16:26 +0530

    setup react router and supabase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
+import { BrowserRouter,Routes, Route } from "react-router-dom";
+import {AdminLogin} from "./components/admins/adminLogin/AdminLogin"
+import {AdminSignup} from "./components/admins/adminSignup/AdminSignup"
+import {UserSignup} from "./components/user/userSignup/UserSignup"
+import {UserLogin} from "./components/user/userLogin/UserLogin"
 
 function App() {
   return (
-    <div className="App">
-     helo
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" />
+      <Route path="/admin/signup" element={<AdminSignup/>}/>
+      <Route path="/admin/signin" element={<AdminLogin/>}/>
+      <Route path="/user/signin" element={<UserLogin/>}/>
+      <Route path="/user/singup" element={<UserSignup/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
